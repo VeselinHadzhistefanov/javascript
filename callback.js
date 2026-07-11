c2cf03dd80c1b2c8fc6bac5fb4f271dfa06637bd
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-07-11 17:48:45 +0300

    App demo uploaded

--- callback.js
+++ callback.js
@@ -2,6 +2,8 @@
 
 //synchorsous
 this.value = 1
+this.states = 0;
+
 function processValue(callback, value) {
   return callback(value)
 }
@@ -14,13 +16,22 @@ function decrement(value) {
   return value - 1;
 }
 
-function incrementValueOfObject(obj) {
+function incrementObjectValue(obj) {
   if (obj.value == null) {
     console.log("value reference of object is " + obj.value)
     return;
   }
   obj.value += 1;
 }
+
+function incrementObjectParameter(obj, name) {
+  if (obj[name] == null) {
+    console.log(name + " reference of object is " + obj[name])
+    return
+  }
+  obj[name] += 1;
+}
+
 
 //function test
 console.log("increment " + increment(1))
@@ -33,5 +44,12 @@ console.log("process this.value " + processValue(increment, this.value))
 console.log("log this.value " + this.value)
 
 //increment value reference in object
-incrementValueOfObject(this)
+incrementObjectValue(this)
 console.log(this.value)
+
+//increment parameter by name in object
+incrementObjectParameter(this, "states")
+console.log(this.states)
+
+//increment null parameter in object
+incrementObjectParameter(this, "state")
