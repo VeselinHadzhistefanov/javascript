ff157251bc8491797bfb3d1ea265bab98d9ecd29
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-07-19 15:07:24 +0300

    fully functional calculator complete

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -9,6 +9,22 @@ class EntryNode {
     this.sy = (sy != null) ? sy : null;
     this.next = (next != null) ? next : null;
   }
+  collapse() {
+    let stack = this
+    let num = 0;
+    let ex = 1;
+    while (stack != null) {
+      if (stack.sy == ".") {
+        num /= ex
+        ex = 1;
+      } else {
+        num += parseInt(stack.sy) * ex
+        ex *= 10;
+      }
+      stack = stack.next
+    }
+    return num
+  }
 }
 
 class OperationNode {
@@ -16,8 +32,8 @@ class OperationNode {
     this.entr = (entr != null) ? entr : null;
     this.op = (op != null) ? op : op;
   }
-  complete = function (entr, op) {
-    switch (op) {
+  complete(entr, op) {
+    switch (this.op) {
       case "+":
         this.entr += entr;
         break;
@@ -41,11 +57,17 @@ let entryMode = EntryMode.WHOLE
 let entryStack = null
 let operation = null
 
-// event listener for numeric buttons
+// numeric buttons event listeners
 const numericButtons = document.getElementsByClassName("numeric-button")
 for (let i = 0; i < numericButtons.length; i++) {
   let btn = numericButtons.item(i)
   btn.addEventListener("click", handleNumericEntry)
+}
+// operation buttons event listeners
+const operationButtons = document.getElementsByClassName("operation-button")
+for (let i = 0; i < operationButtons.length; i++) {
+  let btn = operationButtons.item(i)
+  btn.addEventListener("click", handleOperationEntry)
 }
 
 function handleNumericEntry(e) {
@@ -66,22 +88,27 @@ function handleNumericEntry(e) {
       entryMode = EntryMode.FRACTIONAL
     }
   }
-
-  console.log(collapseNumericStack(entryStack))
+  console.log(entryStack.collapse())
 }
 
-function collapseNumericStack(stack) {
-  let num = 0;
-  let ex = 1;
-  while (stack != null) {
-    if (stack.sy == ".") {
-      num /= ex
-      ex = 1;
-    } else {
-      num += parseInt(stack.sy) * ex
-      ex *= 10;
+function handleOperationEntry(e) {
+  let op = e.srcElement.innerHTML
+  if (entryMode == EntryMode.WHOLE || entryMode == EntryMode.FRACTIONAL) {
+    entryMode = EntryMode.OPERATION
+    if (entryStack == null) {
+      enrtyStack = new EntryNode(0, null);
     }
-    stack = stack.next
+    if (operation == null) {
+      operation = new OperationNode(entryStack.collapse(), op)
+    }
+    else {
+      operation.complete(entryStack.collapse(), op)
+      console.log(operation.entr)
+    }
   }
-  return num
+  else {
+    operation.op = op
+  }
 }
+
+
--- calculator/entry_mode_diagram.md
+++ calculator/entry_mode_diagram.md
@@ -10,4 +10,5 @@ example entry sequence:
 
 rules:
 + operation is always between 2 numbers
-+ number gets put into "first number" spot after an operation is pressed
++ number gets put into "first number" spot after an operation is entered
+
